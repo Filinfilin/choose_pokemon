00db2fb8293e4ca9fea7e6dad25521e99450a50c
Filinfilin <mail.ruslan.filin@gmail.com>
2022-09-16 18:16:59 -0400

    Add react-router-dom

--- react-js-starter/src/App.js
+++ react-js-starter/src/App.js
@@ -1,12 +1,18 @@
 import "./App.scss";
 import Form from "./components/form";
+import PokemonPage from "./components/pokemonPage";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import React from "react";
 
 function App() {
   return (
     <div className="App">
-      <Form />
+      <Routes>
+        <Route exact path="/" element={<Form Link={Link} />}></Route>
+        <Route path="/pokemon-list" element={<PokemonPage />}></Route>
+      </Routes>
     </div>
   );
 }
 
-export default App;
+export default React.memo(App);
