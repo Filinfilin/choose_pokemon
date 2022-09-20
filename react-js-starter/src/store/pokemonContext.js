3136cbd98d121749ce628ee2cd923e37c334f1cb
Filinfilin <mail.ruslan.filin@gmail.com>
2022-09-20 10:47:44 -0400

    Set main state to context

--- react-js-starter/src/store/pokemonContext.js
+++ react-js-starter/src/store/pokemonContext.js
@@ -3,13 +3,51 @@ import { createContext, useState } from "react";
 const PokemonContext = createContext();
 
 export function PokemonProvider({ children }) {
+  const [link, setLink] = useState(null);
   const [movies, setMovies] = useState([]);
+  const [abilities, setAbilities] = useState([]);
+  const [movieFilter, setMovieFilter] = useState({
+    name: "",
+    url: "",
+    result: [],
+  });
+  const [abilityFilter, setAbilityFilter] = useState({
+    name: "",
+    url: "",
+    result: [],
+  });
+  const [names, setNames] = useState({
+    results: [],
+  });
+  const [openDropdown, setOpendropdown] = useState({
+    movie: false,
+    ability: false,
+  });
   const [settings, setSettings] = useState(null);
+  const [result, setResult] = useState([]);
+  const [showSearchResult, setShowSearchResult] = useState(false);
+
   const state = {
     setMovies,
     movies,
     settings,
     setSettings,
+    names,
+    setNames,
+    result,
+    setResult,
+    abilities,
+    setAbilities,
+    showSearchResult,
+    setShowSearchResult,
+    openDropdown,
+    setOpendropdown,
+    abilityFilter,
+    setAbilityFilter,
+    movieFilter,
+    setMovieFilter,
+    link,
+    setLink,
   };
 
   return (
